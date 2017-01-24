0166bb6c46d3642c9677b70c698a544f5715c2ea
jozellrili <jozellrili@gmail.com>
2017-01-24 22:49:10 +0800

    -update files

--- catalog.php
+++ catalog.php
@@ -1,4 +1,12 @@
+<?php
+//check if user has session
+session_start();
+if($_SESSION["username"] == null) { //if not redirect to login page
+    header('location: index.php');
+}
 
+include('data-manager/get-data.php');
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -20,57 +28,7 @@
 
 <body>
 	<div id="wrapper">
-	<!--NAV TOP-->
-        <nav class="navbar navbar-default navbar-cls-top navbar-fixed-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <a href="index.php"/>
-                	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-            </div>
-            
-        </nav>   
-<!-- NAV TOP  -->
-<!-- SIDE BAR  -->
-	<div class="sidebar-wrapper">
-		<nav class="navbar-default navbar-side" role="navigation">
-        	<div class="sidebar-collapse">
-                <ul class="nav" id="main-menu">
-					<a href="index.php">
-                    <li class="text-center">
-                    <img src="assets/images/ops.png" style="height:100px;width: 250px; margin-bottom: 4px; margin-left: 5px; margin-right: 5px;"/>
-					</li></a>
-                    <li>
-                        <a href="admin.php"><img src="assets/images/dashboard.ico" class="img"> Dashboard</a>
-                    </li>
-                    <li>
-                        <a  href="inventory.php"><img src="assets/images/inventory-flat.png" class="img"> Inventory</a>
-                    </li>
-                    <li>
-                        <a  href="catalog.php"><img src="assets/images/catalogue-icon.png" class="img"> Product Catalog</a>
-                    </li>
-                    <li>
-                        <a  href="order-tracking.php"><img src="assets/images/order-tracking.png" class="img"> Order Tracking</a>
-                    </li>
-				    <li  >
-                        <a   href="user.php"><img src="assets/images/user-512.png" class="img"> User Account</a>
-                    </li>	
-                    <li>
-                    	<a href="reports.php"> <img src="assets/images/analytics.png" class="img"> Reports</a>
-                    </li>
-                </ul>  
-            </div> 
-    	</nav>  
-	</div>
-<!--sidebar-->
-<!--End of default-->
-
-<!--contents--> 
- <br><br>     
+        <?php include ('./main-menu.php'); ?>
 	<div id="page-wrapper" >
     	<div id="page-catalog">
         	<div class="row">
