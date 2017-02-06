7775c4c8235fd7ebeea9489d04af603761d53091
Jozell Rili <jozell.rili@gameloft.com>
2017-02-06 12:04:15 +0800

    updated db: added inventory_history table

--- config/ops_db.sql
+++ config/ops_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 01, 2017 at 01:35 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Generation Time: Feb 06, 2017 at 05:02 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 7.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `ops_db`
@@ -26,13 +26,24 @@ SET time_zone = "+00:00";
 -- Table structure for table `inventory`
 --
 
-CREATE TABLE IF NOT EXISTS `inventory` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `inventory` (
+  `id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `stock_date` date NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `stock_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory_history`
+--
+
+CREATE TABLE `inventory_history` (
+  `inventory_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `stock_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -40,12 +51,11 @@ CREATE TABLE IF NOT EXISTS `inventory` (
 -- Table structure for table `logs`
 --
 
-CREATE TABLE IF NOT EXISTS `logs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `logs` (
+  `id` int(11) NOT NULL,
   `activity` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -53,7 +63,7 @@ CREATE TABLE IF NOT EXISTS `logs` (
 -- Table structure for table `order`
 --
 
-CREATE TABLE IF NOT EXISTS `order` (
+CREATE TABLE `order` (
   `id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
@@ -69,22 +79,14 @@ CREATE TABLE IF NOT EXISTS `order` (
 -- Table structure for table `product`
 --
 
-CREATE TABLE IF NOT EXISTS `product` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `price` float(255,2) NOT NULL,
   `picture` text NOT NULL,
-  `category` int(2) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `product`
---
-
-INSERT INTO `product` (`id`, `name`, `description`, `price`, `picture`, `category`) VALUES
-(1, 'bp monitor', 'bp ', 850.00, 'Blood-Pressure-Monitor.jpg', 3);
+  `category` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -92,10 +94,9 @@ INSERT INTO `product` (`id`, `name`, `description`, `price`, `picture`, `categor
 -- Table structure for table `product_gallery`
 --
 
-CREATE TABLE IF NOT EXISTS `product_gallery` (
+CREATE TABLE `product_gallery` (
   `product_id` int(11) NOT NULL,
-  `photo` blob NOT NULL,
-  PRIMARY KEY (`product_id`)
+  `photo` blob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -104,12 +105,11 @@ CREATE TABLE IF NOT EXISTS `product_gallery` (
 -- Table structure for table `receipt`
 --
 
-CREATE TABLE IF NOT EXISTS `receipt` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `receipt` (
+  `id` int(11) NOT NULL,
   `order_id` int(11) NOT NULL,
-  `customer_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `customer_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -117,8 +117,8 @@ CREATE TABLE IF NOT EXISTS `receipt` (
 -- Table structure for table `user_account`
 --
 
-CREATE TABLE IF NOT EXISTS `user_account` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user_account` (
+  `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `first_name` varchar(100) NOT NULL,
@@ -127,21 +127,79 @@ CREATE TABLE IF NOT EXISTS `user_account` (
   `shipping_address` varchar(255) NOT NULL,
   `contact_number` text,
   `email` varchar(255) NOT NULL,
-  `user_role` tinyint(1) NOT NULL COMMENT '0=staff; 1=admin; 2 = customer',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=29 ;
+  `user_role` tinyint(1) NOT NULL COMMENT '0=staff; 1=admin; 2 = customer'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `user_account`
+-- Indexes for dumped tables
 --
 
-INSERT INTO `user_account` (`id`, `username`, `password`, `first_name`, `last_name`, `address`, `shipping_address`, `contact_number`, `email`, `user_role`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'admin', 'Manila PH', '', '09170000000', 'admin@email.com', 1),
-(9, 'juan', '25d55ad283aa400af464c76d713c07ad', 'Juan', 'Dela Cruz', 'sa bahay..', 'sa batangas', '3252352352', 'juandelacruz@email.com', 2),
-(24, 'rootsdadas', '635b6d89912a0c52da2d953ccc468aad', 'adasd', 'sad', 'asdeee', '', '0213457989', 'email@email.com', 1),
-(25, 'test1', '635b6d89912a0c52da2d953ccc468aad', 'test', 'test', 'test', '', '09178025551', 'jozellriili@gmail.com', 1);
+--
+-- Indexes for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product_gallery`
+--
+ALTER TABLE `product_gallery`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `receipt`
+--
+ALTER TABLE `receipt`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_account`
+--
+ALTER TABLE `user_account`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `inventory`
+--
+ALTER TABLE `inventory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `receipt`
+--
+ALTER TABLE `receipt`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user_account`
+--
+ALTER TABLE `user_account`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
