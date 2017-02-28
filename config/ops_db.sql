c9ff8008dc8ddc80e370ed6f60c7be74ff6878a7
jozellrili <jozellrili@gmail.com>
2017-02-28 23:08:42 +0800

    update db

--- config/ops_db.sql
+++ config/ops_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 21, 2017 at 11:32 PM
+-- Generation Time: Feb 28, 2017 at 04:08 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -35,6 +35,19 @@ CREATE TABLE IF NOT EXISTS `inventory` (
   KEY `product_id` (`product_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
 
+--
+-- Dumping data for table `inventory`
+--
+
+INSERT INTO `inventory` (`id`, `product_id`, `quantity`, `stock_date`) VALUES
+(1, 1, 23, '2017-02-18 03:02:53'),
+(2, 2, 6, '2017-02-18 03:28:53'),
+(3, 4, 9, '2017-02-18 03:29:03'),
+(4, 5, 5, '2017-02-18 03:29:12'),
+(5, 7, 10, '2017-02-18 03:29:22'),
+(6, 6, 0, '2017-02-18 03:29:35'),
+(7, 10, 100, '2017-02-19 09:22:42');
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +59,25 @@ CREATE TABLE IF NOT EXISTS `inventory_history` (
   `quantity` int(11) NOT NULL,
   `stock_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `inventory_history`
+--
+
+INSERT INTO `inventory_history` (`inventory_id`, `quantity`, `stock_date`) VALUES
+(1, 10, '2017-02-18 03:02:53'),
+(1, 5, '2017-02-18 03:03:21'),
+(2, 10, '2017-02-18 03:28:53'),
+(3, 10, '2017-02-18 03:29:03'),
+(4, 5, '2017-02-18 03:29:12'),
+(5, 100, '2017-02-18 03:29:22'),
+(6, 1, '2017-02-18 03:29:35'),
+(5, 5, '2017-02-18 14:30:32'),
+(1, 5, '2017-02-19 05:03:18'),
+(1, 5, '2017-02-19 05:03:26'),
+(1, 1, '2017-02-19 05:03:34'),
+(1, 3, '2017-02-19 05:03:42'),
+(7, 100, '2017-02-19 09:22:42');
 
 -- --------------------------------------------------------
 
@@ -73,7 +105,14 @@ CREATE TABLE IF NOT EXISTS `order_details` (
   `price` float(11,2) NOT NULL,
   `quantity` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `order_details`
+--
+
+INSERT INTO `order_details` (`id`, `order_id`, `product_id`, `price`, `quantity`) VALUES
+(1, 1, 1, 1500.00, 1);
 
 -- --------------------------------------------------------
 
@@ -89,7 +128,14 @@ CREATE TABLE IF NOT EXISTS `order_tbl` (
   `order_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `payment_status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0=pending;1=paid',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `order_tbl`
+--
+
+INSERT INTO `order_tbl` (`id`, `customer_id`, `customer_name`, `order_type`, `order_date`, `payment_status`) VALUES
+(1, 30, NULL, 1, '2017-02-27 14:23:45', 1);
 
 -- --------------------------------------------------------
 
@@ -106,9 +152,18 @@ CREATE TABLE IF NOT EXISTS `payment` (
   `payment_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `payment_mode` tinyint(1) NOT NULL COMMENT '0=partial;1=payment',
   `payment_for` tinyint(1) NOT NULL COMMENT '0=order;1=reservation',
+  `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0 = idle; 1 = confirmed; 2=rejected',
   PRIMARY KEY (`id`),
   KEY `order_id` (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`id`, `order_id`, `reservation_id`, `deposit_number`, `deposit_amount`, `payment_date`, `payment_mode`, `payment_for`, `status`) VALUES
+(1, 1, NULL, 123546, 1501.00, '2017-02-27 15:39:13', 1, 0, 2),
+(2, NULL, 1, 2545, 4000.00, '2017-02-27 15:39:32', 0, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -128,6 +183,23 @@ CREATE TABLE IF NOT EXISTS `product` (
   PRIMARY KEY (`id`),
   KEY `id` (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=14 ;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `description`, `price`, `picture`, `category`, `availability`, `phase_out`) VALUES
+(1, 'BP Monitor', 'Digital bp monitor, small, wired,', 1500.00, 'Blood-Pressure-Monitor.jpg', 1, 0, 0),
+(2, 'Nebulizer', 'electric small and durable', 2500.00, 'nebulizer.png', 2, 0, 0),
+(4, 'Oxygen Tank', 'heavy metal', 500.00, 'oxygen-tank.jpg', 5, 0, 0),
+(5, 'Wheelchair (black)', 'black', 9000.00, 'wheelchair.jpg', 9, 1, 1),
+(6, 'Walker Boots', 'black', 25000.00, 'walker-boots.jpg', 9, 1, 1),
+(7, 'Bandage', 'cotton', 150.00, 'bandage.jpg', 2, 0, 0),
+(9, 'gloves', '', 150.00, 'STERILE-LATEX-GLOVES.png', 4, 1, 0),
+(10, 'ear thermometer', 'battery powered', 250.00, 'ear thermometer.png', 2, 0, 0),
+(11, 'head immobilizer', 'red', 5000.00, 'head immobilizer.png', 9, 1, 0),
+(12, 'Cotton', '500pcs', 150.00, 'cotton balls.png', 2, 0, 0),
+(13, 'Hospital bed', 'large', 15000.00, 'hospital bed.png', 8, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -167,7 +239,15 @@ CREATE TABLE IF NOT EXISTS `reservation_details` (
   `price` float(11,2) NOT NULL,
   `quantity` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `reservation_details`
+--
+
+INSERT INTO `reservation_details` (`id`, `reservation_id`, `product_id`, `price`, `quantity`) VALUES
+(1, 1, 1, 1500.00, 1),
+(2, 1, 2, 2500.00, 1);
 
 -- --------------------------------------------------------
 
@@ -184,6 +264,13 @@ CREATE TABLE IF NOT EXISTS `reservation_tbl` (
   `payment_status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0=pending;1=partial;2=full',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `reservation_tbl`
+--
+
+INSERT INTO `reservation_tbl` (`id`, `customer_id`, `customer_name`, `reservation_type`, `reserved_date`, `payment_status`) VALUES
+(1, 30, NULL, 1, '2017-02-27 13:48:20', 1);
 
 -- --------------------------------------------------------
 
@@ -205,6 +292,17 @@ CREATE TABLE IF NOT EXISTS `user_account` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=33 ;
+
+--
+-- Dumping data for table `user_account`
+--
+
+INSERT INTO `user_account` (`id`, `username`, `password`, `first_name`, `last_name`, `address`, `shipping_address`, `contact_number`, `email`, `user_role`) VALUES
+(9, 'juan', '25d55ad283aa400af464c76d713c07ad', 'Juan', 'Dela Cruz', 'sa bahay namin...', 'sa batangas', '3252352352', 'juandelacruz@email.com', 1),
+(25, 'test', '25d55ad283aa400af464c76d713c07ad', 'test', 'test', 'test', '', '09178025551', 'jozellriili@gmail.com', 0),
+(29, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Inoue', 'Kurosaki', 'Karakura town', '', '09175647893', 'email@email.com', 1),
+(30, 'root', 'ac9a61ef2cabc111fa31131b2baebc42', 'jozell', 'rili', 'cainta', 'cainta', '09178025558', 'jozell@email.com', 2),
+(32, 'jho', '635b6d89912a0c52da2d953ccc468aad', 'jho', 'Rili', 'hdskfh', '', '0912456784', 'jho@email.com', 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
