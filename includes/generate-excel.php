d57029d7950d2eeda1c91d7daa368da4d347c040
Jozell Rili <jozell.rili@gameloft.com>
2017-03-14 15:27:54 +0800

    generate excel updates

--- includes/generate-excel.php
+++ includes/generate-excel.php
@@ -2,36 +2,48 @@
 require_once('../plugins/PHPEXcel/Classes/PHPEXcel.php');
 include(dirname(__FILE__).'/../config/db_connection.php');
 
-$query = "SELECT * FROM `invaentory` ";
+$query = "SELECT * FROM `inventory` ";
 $arr = array();
 if ($result = mysqli_query($con, $query)) {
     /* fetch associative array */
-    while ($row = mysqli_fetch_assoc($result)) {
+    while ($row = mysqli_fetch_array($result)) {
         $arr[] = $row;
     }
     /* free result set */
     mysqli_free_result($result);
 }
 
+echo '<pre>'; print_r($arr); exit;
+
 //create new instance of PHPExcel
 $objPHPExcel = new PHPExcel();
-$objPHPExcel->getActiveSheet()->setTitle('List of Users');
 
-$rowNumber = 1;
+// Set properties
+$objPHPExcel->getProperties()->setCreator('Admin');
+$objPHPExcel->getProperties()->setLastModifiedBy('Admin');
+$objPHPExcel->getProperties()->setTitle('Test Excel');
+$objPHPExcel->getProperties()->setSubject('Testing');
+
+$i =1;
 $col = 'A';
 $headings = array('ID', 'Product ID','Qty', 'Stock');
+
 foreach($headings as $heading) {
-	$objPHPExcel->getActiveSheet()->setCellValue($col.$rowNumber,$heading);
+	$objPHPExcel->getActiveSheet()->setCellValue($col.$i,$heading);
 	$col++;
 }
 
-$rowNumber = 2;
-$col = 'A';
-foreach($arr as $cell) {
-    $objPHPExcel->getActiveSheet()->setCellValue($col.$rowNumber,$cell); 
-    $col++;
+foreach($arr as $index => $cell) {
+    $i++;
+    $col = 'A';
+    $objPHPExcel->getActiveSheet()->setCellValue($col.$i,$cell['id']); $col++;
+    $objPHPExcel->getActiveSheet()->setCellValue($col.$i,$cell['product_id']); $col++;
+    $objPHPExcel->getActiveSheet()->setCellValue($col.$i,$cell['quantity']); $col++;
+    $objPHPExcel->getActiveSheet()->setCellValue($col.$i,$cell['stock_date']); 
+
+    
 }
-$rowNumber++;
+
 
 // Save Excel 2007 file
 $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
